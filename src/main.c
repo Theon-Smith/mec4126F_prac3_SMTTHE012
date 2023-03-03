738b163cd2703b47cd8c61e1a2a1118ed1323559
Theon Smith <64099133+Theon-Smith@users.noreply.github.com>
2023-03-03 12:28:22 +0200

    3.1
    Created a global variable "my_age" in the form of a struct "age_data" that holds my date of birth and age

--- src/main.c
+++ src/main.c
@@ -7,9 +7,16 @@
 #define STM32F051												   //COMPULSORY
 #include "stm32f0xx.h"											   //COMPULSORY
 #include "lcd_stm32f0.h"
+#include <stdint.h>
 
 // GLOBAL VARIABLES ----------------------------------------------------------|
 
+struct age_data {
+	uint8_t day;
+	uint8_t month;
+	uint16_t year;
+	uint8_t age;
+} my_age = {17, 8, 2001, 21};
 
 // FUNCTION DECLARATIONS -----------------------------------------------------|
 
